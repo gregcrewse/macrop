907edd22e3bb09467f204031024dfb4166f7810e
gregcrewse <gregcrewse@gmail.com>
2025-03-20 09:44:37 -0400

    Update simple_version.sql

--- simple_version.sql
+++ simple_version.sql
@@ -1,51 +1,55 @@
--- macros/simple_compare_models.sql
-{% macro simple_compare_models(old_model_name, new_model_name, key_column) %}
+-- macros/redshift_safe_compare.sql
+{% macro redshift_safe_compare(old_model_name, new_model_name, key_column) %}
 
--- Count records in each model
-WITH old_count AS (
-  SELECT COUNT(*) as record_count FROM {{ ref(old_model_name) }}
+-- Get the row counts separately to avoid correlated subqueries
+WITH old_row_count AS (
+  SELECT COUNT(*) as count FROM {{ ref(old_model_name) }}
 ),
 
-new_count AS (
-  SELECT COUNT(*) as record_count FROM {{ ref(new_model_name) }}
+new_row_count AS (
+  SELECT COUNT(*) as count FROM {{ ref(new_model_name) }}
 ),
 
--- Find key values in old but not in new
-missing_in_new AS (
-  SELECT {{ key_column }}, COUNT(*) as count
+-- Use LEFT JOINs instead of NOT EXISTS
+missing_keys_in_new AS (
+  SELECT 
+    source_model.{{ key_column }},
+    1 as is_missing
   FROM {{ ref(old_model_name) }} as source_model
-  WHERE NOT EXISTS (
-    SELECT 1 
-    FROM {{ ref(new_model_name) }} as target_model
-    WHERE target_model.{{ key_column }} = source_model.{{ key_column }}
-  )
-  GROUP BY {{ key_column }}
+  LEFT JOIN {{ ref(new_model_name) }} as target_model
+    ON source_model.{{ key_column }} = target_model.{{ key_column }}
+  WHERE target_model.{{ key_column }} IS NULL
 ),
 
--- Find key values in new but not in old
-missing_in_old AS (
-  SELECT {{ key_column }}, COUNT(*) as count
+missing_keys_in_old AS (
+  SELECT 
+    target_model.{{ key_column }},
+    1 as is_missing
   FROM {{ ref(new_model_name) }} as target_model
-  WHERE NOT EXISTS (
-    SELECT 1 
-    FROM {{ ref(old_model_name) }} as source_model
-    WHERE source_model.{{ key_column }} = target_model.{{ key_column }}
-  )
-  GROUP BY {{ key_column }}
+  LEFT JOIN {{ ref(old_model_name) }} as source_model
+    ON target_model.{{ key_column }} = source_model.{{ key_column }}
+  WHERE source_model.{{ key_column }} IS NULL
 ),
 
--- Summary statistics
+-- Count totals of missing keys
+missing_counts AS (
+  SELECT
+    (SELECT COUNT(*) FROM missing_keys_in_new) as keys_missing_in_new,
+    (SELECT COUNT(*) FROM missing_keys_in_old) as keys_missing_in_old
+),
+
+-- Calculate final summary
 summary AS (
   SELECT
     '{{ old_model_name }}' as old_model,
     '{{ new_model_name }}' as new_model,
-    (SELECT record_count FROM old_count) as old_record_count,
-    (SELECT record_count FROM new_count) as new_record_count,
-    (SELECT COUNT(*) FROM missing_in_new) as keys_missing_in_new,
-    (SELECT COUNT(*) FROM missing_in_old) as keys_missing_in_old
+    (SELECT count FROM old_row_count) as old_record_count,
+    (SELECT count FROM new_row_count) as new_record_count,
+    mc.keys_missing_in_new,
+    mc.keys_missing_in_old
+  FROM missing_counts mc
 )
 
--- Return the summary
 SELECT * FROM summary
 
 {% endmacro %}
